b870f06478a76e49fd35fe5a6b94a32e59ba66e6
entbappy <entbappy73@gmail.com>
2023-10-05 22:37:10 +0600

    updated

--- telebot.py
+++ telebot.py
@@ -92,4 +92,4 @@ async def chatgpt(message: types.Message):
 
 
 if __name__ == "__main__":
-    executor.start_polling(dispatcher, skip_updates=True)
+    executor.start_polling(dispatcher, skip_updates=False)
